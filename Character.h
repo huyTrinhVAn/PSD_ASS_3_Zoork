f941b6cb38df786dceea3dae2d8b62350e66343b
huyTrinhVAn <trinhvanhuy11a9@gmail.com>
2025-05-24 22:46:38 +1000

    my first commit

---
+++ Character.h
@@ -0,0 +1,26 @@
+//
+// Created by Richard Skarbez on 5/7/23.
+//
+#include <memory>
+#ifndef ZOORK_CHARACTER_H
+#define ZOORK_CHARACTER_H
+
+#include "GameObject.h"
+#include "Item.h"
+#include <vector>
+
+class Character : public GameObject
+{
+public:
+    Character(const std::string &, const std::string &);
+
+protected:
+    //    std::vector<Item*> inventory;
+    //    std::vector<std::string> tags;
+    //    int health;
+    //    int attack;
+    //    int move;
+    //    int initiative;
+};
+
+#endif // ZOORK_CHARACTER_H
